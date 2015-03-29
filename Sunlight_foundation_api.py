4e2c7f38a7d6cc8acc0dd68c8faa3dced49a59b3
Prabhath Kiran <prabhathkiran20@gmail.com>
2015-03-29 11:31:19 -0700

    Create Sunlight_foundation_api.py

---
+++ Sunlight_foundation_api.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 16:57:43 2015
+
+@author: prabhath
+"""
+
+import requests
+import json
+
+query_params = { 'apikey': '148ea5f5075c4b54b0e55bfa0d59bce1',
+                'per_page': 3,
+                'phrase': 'congratulations to you!',
+                'sort': 'date desc'
+                }		       
+endpoint = 'http://capitolwords.org/api/text.json'
+
+response = requests.get( endpoint, params=query_params)
+response.text
+
+data = json.loads(response.text)
+print json.dumps(data["results"][0])
